1854a12bb0f958b677dcc78a1cefcf40469b0485
kon28289 <kon28289@naver.com>
2025-07-28 15:53:06 +0900

    feat: JPA Auditing을 위한 어노테이션 및 properties를 빈으로 등록하기 위한 어노테이션 추가

--- src/main/java/com/uhdyl/backend/UhDyLBackendApplication.java
+++ src/main/java/com/uhdyl/backend/UhDyLBackendApplication.java
@@ -1,9 +1,14 @@
 package com.uhdyl.backend;
 
+import com.uhdyl.backend.global.oauth.user.KakaoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableConfigurationProperties(KakaoProperties.class)
 public class UhDyLBackendApplication {
 
     public static void main(String[] args) {

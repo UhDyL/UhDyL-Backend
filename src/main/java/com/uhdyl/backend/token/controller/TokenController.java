76566ceb8e214775e78e39e8224e36a4591d15c6
kon28289 <kon28289@naver.com>
2025-07-28 15:52:22 +0900

    feat: token 도메인 추가

---
+++ src/main/java/com/uhdyl/backend/token/controller/TokenController.java
@@ -0,0 +1,32 @@
+package com.uhdyl.backend.token.controller;
+
+
+import com.uhdyl.backend.global.response.ResponseBody;
+import com.uhdyl.backend.token.api.TokenApi;
+import com.uhdyl.backend.token.dto.request.TokenRequest;
+import com.uhdyl.backend.token.dto.response.TokenResponse;
+import com.uhdyl.backend.token.entity.Token;
+import com.uhdyl.backend.token.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.uhdyl.backend.global.response.ResponseUtil.createSuccessResponse;
+
+
+@RestController
+@RequestMapping("/auth/token")
+@RequiredArgsConstructor
+public class TokenController implements TokenApi {
+    private final TokenService tokenService;
+
+    @PostMapping("/refresh")
+    public ResponseEntity<ResponseBody<TokenResponse>> refresh(@RequestBody TokenRequest tokenRequest) {
+        Token token = new Token(tokenRequest.accessToken(), tokenRequest.refreshToken());
+        TokenResponse response = tokenService.refresh(token);
+        return ResponseEntity.ok(createSuccessResponse(response));
+    }
+}
---
+++ src/main/java/com/uhdyl/backend/token/service/TokenService.java
@@ -0,0 +1,41 @@
+package com.uhdyl.backend.token.service;
+
+
+import com.uhdyl.backend.global.exception.BusinessException;
+import com.uhdyl.backend.global.exception.ExceptionType;
+import com.uhdyl.backend.global.jwt.JwtHandler;
+import com.uhdyl.backend.global.jwt.JwtUserClaim;
+import com.uhdyl.backend.token.dto.response.TokenResponse;
+import com.uhdyl.backend.token.entity.RefreshToken;
+import com.uhdyl.backend.token.entity.Token;
+import com.uhdyl.backend.token.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final JwtHandler jwtHandler;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Transactional
+    public TokenResponse refresh(Token token) {
+        JwtUserClaim jwtUserClaim = jwtHandler.getClaims(token.getAccessToken())
+                .orElseThrow(() -> new BusinessException(ExceptionType.JWT_INVALID)); // invalid token
+
+        RefreshToken savedRefreshToken = refreshTokenRepository.findByUserId(jwtUserClaim.userId())
+                .orElseThrow(() -> new BusinessException(ExceptionType.REFRESH_TOKEN_NOT_EXIST)); // not exist token
+
+        if(!token.getRefreshToken().equals(savedRefreshToken.getRefreshToken())) // userId 비교
+            throw new BusinessException(ExceptionType.TOKEN_NOT_MATCHED);
+
+        refreshTokenRepository.deleteByUserId(savedRefreshToken.getUserId());
+
+        Token tokenResponse = jwtHandler.createTokens(jwtUserClaim);
+        return new TokenResponse(tokenResponse.getAccessToken(), tokenResponse.getRefreshToken());
+    }
+
+
+}

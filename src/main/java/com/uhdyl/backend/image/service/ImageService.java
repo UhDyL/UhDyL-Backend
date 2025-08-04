e7f32a5861a5db87c87f06a853e0b77916fcd127
kon28289 <kon28289@naver.com>
2025-08-04 19:05:10 +0900

    chore: 예외 추가 및 오류 수정

--- src/main/java/com/uhdyl/backend/image/service/ImageService.java
+++ src/main/java/com/uhdyl/backend/image/service/ImageService.java
@@ -7,6 +7,7 @@ import com.uhdyl.backend.chat.repository.ChatMessageRepository;
 import com.uhdyl.backend.global.exception.BusinessException;
 import com.uhdyl.backend.global.exception.ExceptionType;
 import com.uhdyl.backend.image.domain.ImageType;
+import com.uhdyl.backend.image.dto.request.ImageDeleteRequest;
 import com.uhdyl.backend.image.dto.response.ImageSavedSuccessResponse;
 import com.uhdyl.backend.image.repository.ImageRepository;
 import com.uhdyl.backend.user.domain.User;
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -30,6 +32,13 @@ public class ImageService {
     private final ChatMessageRepository chatMessageRepository;
 
     public ImageSavedSuccessResponse uploadImage(MultipartFile image, String folderPath){
+
+        if(image == null || image.isEmpty())
+            throw new BusinessException(ExceptionType.INVALID_IMAGE_FILE);
+
+        if(image.getSize() > 1024 * 1024 * 5)
+            throw new BusinessException(ExceptionType.IMAGE_SIZE_EXCEEDED);
+
         try {
             Map<?, ?> result = cloudinary.uploader().upload(
                     image.getBytes(),
@@ -38,6 +47,10 @@ public class ImageService {
                             "public_id", UUID.randomUUID().toString()
                     )
             );
+
+            if(result.get("secure_url") == null || result.get("public_id") == null)
+                throw new BusinessException(ExceptionType.IMAGE_UPLOAD_FAILED);
+
             return ImageSavedSuccessResponse.to(result.get("secure_url").toString(), result.get("public_id").toString());
         } catch (IOException e) {
             throw new BusinessException(ExceptionType.IMAGE_UPLOAD_FAILED);
@@ -45,38 +58,44 @@ public class ImageService {
     }
 
     @Transactional
-    public void deleteImage(ImageType imageType, String publicId, Long userId){
+    public void deleteImage(List<ImageDeleteRequest> request, Long userId){
 
         // TODO: 리뷰 도메인 추가 시 로직 추가하기
-        switch (imageType){
-            case USER_IMAGE -> {
-                if(userRepository.existsByIdAndPublicId(userId, publicId)){
+        request.forEach(imageDeleteRequest -> {
+            ImageType imageType = imageDeleteRequest.imageType();
+            String publicId = imageDeleteRequest.publicId();
+            switch (imageType){
+                case USER_IMAGE -> {
+                    if(!userRepository.existsByIdAndPublicId(userId, publicId))
+                        throw new BusinessException(ExceptionType.IMAGE_ACCESS_DENIED);
+
                     User user = userRepository.findById(userId)
                             .orElseThrow(() -> new BusinessException(ExceptionType.USER_NOT_FOUND));
                     user.deleteImage();
                 }
-            }
 
-            case PRODUCT_IMAGE -> {
-                if (imageRepository.existsByUserIdAndPublicId(userId, publicId)){
+                case PRODUCT_IMAGE -> {
+                    if (!imageRepository.existsByUserIdAndPublicId(userId, publicId))
+                        throw new BusinessException(ExceptionType.IMAGE_ACCESS_DENIED);
+
                     imageRepository.deleteByPublicId(publicId);
                 }
-            }
 
-            case CHAT_IMAGE -> {
-                if (chatMessageRepository.existsByUserIdAndPublicId(userId, publicId)){
+                case CHAT_IMAGE -> {
+                    if (!chatMessageRepository.existsByUserIdAndPublicId(userId, publicId))
+                        throw new BusinessException(ExceptionType.IMAGE_ACCESS_DENIED);
+
                     ChatMessage chatMessage = chatMessageRepository.findByPublicId(publicId);
                     chatMessage.deleteImage();
                 }
             }
-        }
 
-        try {
-            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
-        } catch (IOException e) {
-            throw new BusinessException(ExceptionType.IMAGE_DELETE_FAILED);
-        }
-
+            try {
+                cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+            } catch (IOException e) {
+                throw new BusinessException(ExceptionType.IMAGE_DELETE_FAILED);
+            }
+        });
 
     }
 

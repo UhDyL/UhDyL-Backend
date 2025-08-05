6a3a1eb1a1ab1c2b799dd6a78267a10d935e2837
kon28289 <kon28289@naver.com>
2025-08-05 18:48:13 +0900

    docs: 컨트롤러 추가

---
+++ src/main/java/com/uhdyl/backend/review/controller/ReviewController.java
@@ -0,0 +1,82 @@
+package com.uhdyl.backend.review.controller;
+
+import com.uhdyl.backend.global.aop.AssignUserId;
+import com.uhdyl.backend.global.response.ResponseBody;
+import com.uhdyl.backend.review.api.ReviewApi;
+import com.uhdyl.backend.review.dto.request.ReviewCreateRequest;
+import com.uhdyl.backend.review.dto.response.ReviewResponse;
+import com.uhdyl.backend.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import static com.uhdyl.backend.global.response.ResponseUtil.createSuccessResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewController implements ReviewApi {
+
+    private final ReviewService reviewService;
+
+    /**
+     * 리뷰 작성 api
+     */
+    @PostMapping("/review")
+    @AssignUserId
+    @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+    public ResponseEntity<ResponseBody<Void>> createReview(
+            Long userId,
+            @RequestBody ReviewCreateRequest reviewCreateRequest
+    )
+    {
+        reviewService.createReview(userId, reviewCreateRequest);
+        return ResponseEntity.ok(createSuccessResponse());
+    }
+
+    /**
+     *  자신이 작성한 리뷰 페이징 조회 api
+     *  구매자가 사용할 api
+     */
+    @AssignUserId
+    @GetMapping("/review/me")
+    @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+    public ResponseEntity<ResponseBody<Page<ReviewResponse>>> getMyReviews(
+            Long userId,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ){
+        return ResponseEntity.ok(createSuccessResponse(reviewService.getMyReviews(userId, pageable)));
+    }
+
+    /**
+     *  자신에게 작성된 리뷰 페이징 조회 api
+     *  판매자가 사용할 api
+     */
+    @AssignUserId
+    @GetMapping("/review")
+    @PreAuthorize("isAuthenticated() and hasAuthority('FARMER')")
+    public ResponseEntity<ResponseBody<Page<ReviewResponse>>> getAllReviews(
+            Long userId,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ){
+        return ResponseEntity.ok(createSuccessResponse(reviewService.getAllReviews(userId, pageable)));
+    }
+
+    /**
+     * 리뷰 삭제 api
+     */
+    @DeleteMapping("/review/{reviewId}")
+    @AssignUserId
+    @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+    public ResponseEntity<ResponseBody<Void>> deleteReview(
+            Long userId,
+            @PathVariable Long reviewId
+    ) {
+        reviewService.deleteReview(userId, reviewId);
+        return ResponseEntity.ok(createSuccessResponse());
+    }
+}

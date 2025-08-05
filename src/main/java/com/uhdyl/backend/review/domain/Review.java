17408b49e00fe317c8a298475b0cd9b6644befab
kon28289 <kon28289@naver.com>
2025-08-05 18:48:34 +0900

    docs: 도메인 추가

---
+++ src/main/java/com/uhdyl/backend/review/domain/Review.java
@@ -0,0 +1,45 @@
+package com.uhdyl.backend.review.domain;
+
+import com.uhdyl.backend.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String content;
+
+    private String imageUrl;
+
+    private String publicId;
+
+    private Long rating;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Long targetUserId;
+
+    @Builder
+    public Review(User user, String content, String imageUrl, String publicId, Long rating, Long targetUserId) {
+        this.user = user;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.publicId = publicId;
+        this.rating = rating;
+        this.targetUserId = targetUserId;
+    }
+
+    public void setUser(User user){
+        this.user = user;
+    }
+}

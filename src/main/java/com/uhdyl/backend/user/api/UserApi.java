f2b0de8bd5e8b3722e26fae15ef3820688558512
AYEOOON <k93853471@gmail.com>
2025-08-06 00:25:39 +0900

    feat: 밭 위치 저장하는 API 추가

--- src/main/java/com/uhdyl/backend/user/api/UserApi.java
+++ src/main/java/com/uhdyl/backend/user/api/UserApi.java
@@ -7,16 +7,15 @@ import com.uhdyl.backend.global.config.swagger.SwaggerApiResponses;
 import com.uhdyl.backend.global.config.swagger.SwaggerApiSuccessResponse;
 import com.uhdyl.backend.global.exception.ExceptionType;
 import com.uhdyl.backend.global.response.ResponseBody;
+import com.uhdyl.backend.user.dto.request.LocationRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "사용자 API", description = "사용자 관련 API")
 public interface UserApi {
@@ -39,7 +38,21 @@ public interface UserApi {
     @PreAuthorize(" isAuthenticated() and hasAuthority('USER')")
     public ResponseEntity<ResponseBody<Void>> logout(@Parameter(hidden = true) Long userId);
 
-
-
-
+    @Operation(
+            summary =  "판매자 밭 위치 저장",
+            description = "FARMER 권한을 가진 사용자의 위치를 저장합니다."
+    )
+    @SwaggerApiResponses(
+            success = @SwaggerApiSuccessResponse(description = "위치 저장 성공"),
+            errors = {
+                    @SwaggerApiFailedResponse(ExceptionType.NEED_AUTHORIZED),
+                    @SwaggerApiFailedResponse(ExceptionType.USER_NOT_FOUND),
+                    @SwaggerApiFailedResponse(ExceptionType.USER_NOT_FARMER)
+            }
+    )
+    @AssignUserId
+    @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+    @PostMapping("/location")
+    ResponseEntity<ResponseBody<Void>> saveLocation(@RequestBody LocationRequest request,
+                                                    @Parameter(hidden = true) Long userId);
 }

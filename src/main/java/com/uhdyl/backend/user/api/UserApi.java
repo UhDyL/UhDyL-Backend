3751e178ccc549fba6b3752d5d1da7edd45a35ff
kon28289 <kon28289@naver.com>
2025-08-06 18:48:38 +0900

    docs: 스웨거 문서화

--- src/main/java/com/uhdyl/backend/user/api/UserApi.java
+++ src/main/java/com/uhdyl/backend/user/api/UserApi.java
@@ -7,15 +7,21 @@ import com.uhdyl.backend.global.config.swagger.SwaggerApiResponses;
 import com.uhdyl.backend.global.config.swagger.SwaggerApiSuccessResponse;
 import com.uhdyl.backend.global.exception.ExceptionType;
 import com.uhdyl.backend.global.response.ResponseBody;
+import com.uhdyl.backend.token.dto.response.TokenResponse;
 import com.uhdyl.backend.user.dto.request.LocationRequest;
+import com.uhdyl.backend.user.dto.request.UserNicknameUpdateRequest;
+import com.uhdyl.backend.user.dto.request.UserProfileUpdateRequest;
+import com.uhdyl.backend.user.dto.response.UserProfileResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 
 @Tag(name = "사용자 API", description = "사용자 관련 API")
 public interface UserApi {
@@ -54,5 +60,95 @@ public interface UserApi {
     @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
     @PostMapping("/location")
     ResponseEntity<ResponseBody<Void>> saveLocation(@RequestBody LocationRequest request,
-                                                    @Parameter(hidden = true) Long userId);
+                                                             @Parameter(hidden = true) Long userId);
+
+
+    @Operation(
+            summary = "밭 등록 후 판매자 권한(FARMER) 획득",
+            description = "사용자는 밭 등록 후 판매자 권한(FARMER)를 획득하고 액세스 토큰과 리프래시 토큰을 재발급 받습니다."
+    )
+    @ApiResponse(content = @Content(schema = @Schema(implementation = TokenResponse.class)))
+    @SwaggerApiResponses(
+            success = @SwaggerApiSuccessResponse(
+                    response = TokenResponse.class,
+                    description = "판매자 권한(FARMER) 획득 성공"
+            ),
+            errors = {
+                    @SwaggerApiFailedResponse(ExceptionType.NEED_AUTHORIZED),
+                    @SwaggerApiFailedResponse(ExceptionType.USER_NOT_FOUND),
+                    @SwaggerApiFailedResponse(ExceptionType.BBAT_NOT_UPDATED),
+            }
+    )
+    @AssignUserId
+    @PostMapping("/complete-registration")
+    @PreAuthorize(" isAuthenticated() and hasAuthority('USER')")
+    public ResponseEntity<ResponseBody<TokenResponse>> completeRegistration(
+            @Parameter(hidden = true) Long userId
+    );
+
+
+    @Operation(
+            summary =  "닉네임 변경",
+            description = "사용자는 초기 닉네임을 변경합니다. 이후의 닉네임 변경은 프로필 정보 수정 api를 통해 진행합니다."
+    )
+    @SwaggerApiResponses(
+            success = @SwaggerApiSuccessResponse(description = "닉네임 변경 성공"),
+            errors = {
+                    @SwaggerApiFailedResponse(ExceptionType.NEED_AUTHORIZED),
+                    @SwaggerApiFailedResponse(ExceptionType.USER_NOT_FOUND),
+                    @SwaggerApiFailedResponse(ExceptionType.USER_NICKNAME_DUPLICATED)
+            }
+    )
+    @AssignUserId
+    @PostMapping("/nickname")
+    @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+    public ResponseEntity<ResponseBody<Void>> updateNickname(
+            @Parameter(hidden = true) Long userId,
+            @RequestBody UserNicknameUpdateRequest request
+    );
+
+
+    @Operation(
+            summary = "사용자 프로필 조회",
+            description = "사용자는 자신의 프로필 정보를 조회합니다."
+    )
+    @ApiResponse(content = @Content(schema = @Schema(implementation = UserProfileResponse.class)))
+    @SwaggerApiResponses(
+            success = @SwaggerApiSuccessResponse(
+                    response = UserProfileResponse.class,
+                    description = "프로필 조회 성공"
+            ),
+            errors = {
+                    @SwaggerApiFailedResponse(ExceptionType.NEED_AUTHORIZED),
+                    @SwaggerApiFailedResponse(ExceptionType.USER_NOT_FOUND),
+            }
+    )
+    @AssignUserId
+    @GetMapping("/profile")
+    @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+    public ResponseEntity<ResponseBody<UserProfileResponse>> getProfile(
+            @Parameter(hidden = true) Long userId
+    );
+
+
+    @Operation(
+            summary = "사용자 프로필 수정",
+            description = "사용자는 자신의 프로필 정보를 수정합니다."
+    )
+    @SwaggerApiResponses(
+            success = @SwaggerApiSuccessResponse(
+                    description = "프로필 수정 성공"
+            ),
+            errors = {
+                    @SwaggerApiFailedResponse(ExceptionType.NEED_AUTHORIZED),
+                    @SwaggerApiFailedResponse(ExceptionType.USER_NOT_FOUND),
+            }
+    )
+    @AssignUserId
+    @PatchMapping("/profile")
+    @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+    public ResponseEntity<ResponseBody<Void>> updateProfile(
+            @Parameter(hidden = true) Long userId,
+            @RequestBody UserProfileUpdateRequest request
+    );
 }

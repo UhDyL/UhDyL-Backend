5634af077b5b585497a87c5cf3dd208f1462c914
AYEOOON <k93853471@gmail.com>
2025-08-08 23:41:50 +0900

    feat: 판매자의 위치 조회 컨트롤러 추가

--- src/main/java/com/uhdyl/backend/user/controller/UserController.java
+++ src/main/java/com/uhdyl/backend/user/controller/UserController.java
@@ -5,12 +5,14 @@ import com.uhdyl.backend.global.aop.AssignUserId;
 import com.uhdyl.backend.global.response.ResponseBody;
 import com.uhdyl.backend.user.api.UserApi;
 import com.uhdyl.backend.user.dto.request.LocationRequest;
+import com.uhdyl.backend.user.dto.response.LocationResponse;
 import com.uhdyl.backend.user.service.UserService;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,7 +41,15 @@ public class UserController implements UserApi {
     @PreAuthorize(" isAuthenticated() and hasAuthority('USER')")
     public ResponseEntity<ResponseBody<Void>> saveLocation(@RequestBody LocationRequest request,
                                                            @Parameter(hidden = true) Long userId){
-        userService.saveLocation(userId, request.getLocation_x(), request.getLocation_y());
+        userService.saveLocation(userId, request.location_x(), request.location_y());
         return ResponseEntity.ok(createSuccessResponse());
+    }
+
+    @AssignUserId
+    @GetMapping("/location")
+    @PreAuthorize(" isAuthenticated() and hasAuthority('USER')")
+    public ResponseEntity<ResponseBody<LocationResponse>> getLocation(@Parameter(hidden = true) Long userId){
+        LocationResponse location = userService.getLocation(userId);
+        return ResponseEntity.ok(createSuccessResponse(location));
     }
 }

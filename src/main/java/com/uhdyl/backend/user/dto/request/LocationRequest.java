e28d5b08dbb0b48d2b9c8c2df86554546efeef37
AYEOOON <k93853471@gmail.com>
2025-08-08 23:38:18 +0900

    refactor: LocationRequest DTO를 클래스에서 record로 변경

--- src/main/java/com/uhdyl/backend/user/dto/request/LocationRequest.java
+++ src/main/java/com/uhdyl/backend/user/dto/request/LocationRequest.java
@@ -1,10 +1,8 @@
 package com.uhdyl.backend.user.dto.request;
 
 import java.math.BigDecimal;
-import lombok.Getter;
 
-@Getter
-public class LocationRequest {
-    private BigDecimal location_x;
-    private BigDecimal location_y;
-}
+public record LocationRequest(
+    BigDecimal location_x,
+    BigDecimal location_y
+){}

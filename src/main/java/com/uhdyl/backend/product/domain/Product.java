40f00c945ca94d6fbef5de4f73156c5cbe5e1fe0
AYEOOON <k93853471@gmail.com>
2025-08-09 00:18:46 +0900

    feat: Product 엔티티 생성
    - 상품 엔티티에서 User를 @ManyToOne으로 매핑

---
+++ src/main/java/com/uhdyl/backend/product/domain/Product.java
@@ -0,0 +1,63 @@
+package com.uhdyl.backend.product.domain;
+
+import com.uhdyl.backend.image.domain.Image;
+import com.uhdyl.backend.user.domain.User;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String title;
+
+    @Lob
+    private String description;
+
+    private boolean isSale;
+
+    private String price;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public Product(String name, String title, String description, boolean isSale, String price, Category category, User user) {
+        this.name = name;
+        this.title = title;
+        this.description = description;
+        this.isSale = isSale;
+        this.price = price;
+        this.category = category;
+        this.user = user;
+    }
+}

7e3126b6e1196eca9d96340b60523a7c4b67efcc
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2025-08-09 18:51:58 +0900

    fix: 중괄호 이슈 수정
    - removeProduct 메서드 닫는 중괄호(}) 추가..^^

--- src/main/java/com/uhdyl/backend/user/domain/User.java
+++ src/main/java/com/uhdyl/backend/user/domain/User.java
@@ -105,6 +105,7 @@ public class User extends BaseEntity {
         if (product == null) return;
         this.products.remove(product);
         product.setUser(null);
+    }
 
     public void updateNickname(String nickname){
         this.nickname = nickname;

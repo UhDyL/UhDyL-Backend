bc47317e358eb15725ae96765094f6114cf3c7de
AYEOOON <k93853471@gmail.com>
2025-08-12 00:04:40 +0900

    feat: Product 빌더에 id 추가

--- src/main/java/com/uhdyl/backend/product/domain/Product.java
+++ src/main/java/com/uhdyl/backend/product/domain/Product.java
@@ -57,7 +57,8 @@ public class Product extends BaseEntity {
     private User user;
 
     @Builder
-    public Product(String name, String title, String description, boolean isSale, String price, Category category, User user) {
+    public Product(Long id, String name, String title, String description, boolean isSale, String price, Category category, User user) {
+        this.id = id;
         this.name = name;
         this.title = title;
         this.description = description;

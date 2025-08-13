88ac32471c42234489f5d83876e44fa1339be8c5
AYEOOON <k93853471@gmail.com>
2025-08-14 00:46:26 +0900

    feat: ProductRepository에 findByIdAndUserId 메서드 추가

--- src/main/java/com/uhdyl/backend/product/repository/ProductRepository.java
+++ src/main/java/com/uhdyl/backend/product/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.uhdyl.backend.product.repository;
 
 
 import com.uhdyl.backend.product.domain.Product;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, CustomProductRepository {
+    Optional<Product> findByIdAndUserId(Long productId, Long userId);
 }

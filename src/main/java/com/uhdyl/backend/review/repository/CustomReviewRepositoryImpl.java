f2dd376b311f4a9256a4014cdd81856adfaa4e13
kon28289 <kon28289@naver.com>
2025-08-17 14:50:49 +0900

    chore: 레파지토리 메서드 수정

--- src/main/java/com/uhdyl/backend/review/repository/CustomReviewRepositoryImpl.java
+++ src/main/java/com/uhdyl/backend/review/repository/CustomReviewRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.uhdyl.backend.review.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.uhdyl.backend.global.exception.BusinessException;
 import com.uhdyl.backend.global.exception.ExceptionType;
+import com.uhdyl.backend.product.domain.QProduct;
 import com.uhdyl.backend.review.domain.QReview;
 import com.uhdyl.backend.review.domain.Review;
 import com.uhdyl.backend.review.dto.response.ReviewResponse;
@@ -25,13 +27,28 @@ public class CustomReviewRepositoryImpl implements CustomReviewRepository{
     @Override
     public Page<ReviewResponse> getMyReviews(Long userId, Pageable pageable) {
         QReview qReview = QReview.review;
+        QProduct qProduct = QProduct.product;
         BooleanBuilder builder = new BooleanBuilder();
 
         builder.and(qReview.user.id.eq(userId));
 
-        List<Review> reviews = jpaQueryFactory
-                .selectFrom(qReview)
+        List<ReviewResponse> responses = jpaQueryFactory
+                .select(
+                        Projections.constructor(
+                                ReviewResponse.class,
+                                qReview.id,
+                                qReview.content,
+                                qReview.rating,
+                                qReview.user.nickname,
+                                qReview.imageUrl,
+                                qProduct.title,
+                                qReview.createdAt
+                        )
+                )
+                .from(qReview)
+                .leftJoin(qProduct).on(qReview.productId.eq(qProduct.id))
                 .where(builder)
+                .orderBy(qReview.createdAt.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
@@ -42,21 +59,33 @@ public class CustomReviewRepositoryImpl implements CustomReviewRepository{
                 .where(builder)
                 .fetchOne();
 
-        List<ReviewResponse> response = reviews.stream().map(ReviewResponse::to).toList();
-
-        return new PageImpl<>(response, pageable, total != null ? total : 0);
+        return new PageImpl<>(responses, pageable, total != null ? total : 0);
     }
 
     @Override
     public Page<ReviewResponse> getAllReviews(Long userId, Pageable pageable) {
         QReview qReview = QReview.review;
+        QProduct qProduct = QProduct.product;
         BooleanBuilder builder = new BooleanBuilder();
 
         builder.and(qReview.targetUserId.eq(userId));
 
-        List<Review> reviews = jpaQueryFactory
-                .selectFrom(qReview)
+        List<ReviewResponse> responses = jpaQueryFactory
+                .select(
+                        Projections.constructor(
+                        ReviewResponse.class,
+                        qReview.id,
+                        qReview.content,
+                        qReview.rating,
+                        qReview.user.nickname,
+                        qReview.imageUrl,
+                        qProduct.title,
+                        qReview.createdAt
+                ))
+                .from(qReview)
+                .leftJoin(qProduct).on(qReview.productId.eq(qProduct.id))
                 .where(builder)
+                .orderBy(qReview.createdAt.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
@@ -67,25 +96,6 @@ public class CustomReviewRepositoryImpl implements CustomReviewRepository{
                 .where(builder)
                 .fetchOne();
 
-        List<ReviewResponse> response = reviews.stream().map(ReviewResponse::to).toList();
-
-        return new PageImpl<>(response, pageable, total != null ? total : 0);
-    }
-
-    @Override
-    public boolean existsByUserIdAndPublicId(Long userId, String publicId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new BusinessException(ExceptionType.USER_NOT_FOUND));
-
-        QReview qReview = QReview.review;
-        BooleanBuilder builder = new BooleanBuilder();
-
-        builder.and(qReview.user.id.eq(userId));
-        builder.and(qReview.publicId.eq(publicId));
-
-        return jpaQueryFactory
-                .selectFrom(qReview)
-                .where(builder)
-                .fetchOne() != null;
+        return new PageImpl<>(responses, pageable, total != null ? total : 0);
     }
 }

4e4d509d15fe08edf7924ad80f29c1403032130a
kon28289 <kon28289@naver.com>
2025-08-20 22:12:57 +0900

    feat: 채팅방 목록 반환 시 거래 완료 여부도 함께 전달

--- src/main/java/com/uhdyl/backend/chat/repository/CustomChatRoomRepositoryImpl.java
+++ src/main/java/com/uhdyl/backend/chat/repository/CustomChatRoomRepositoryImpl.java
@@ -62,7 +62,8 @@ public class CustomChatRoomRepositoryImpl implements CustomChatRoomRepository{
                                 qProduct.isSale
                         ),
                         qChatMessage.message,
-                        qChatMessage.createdAt
+                        qChatMessage.createdAt,
+                        qChatRoom.tradeCompleted
                 ))
                 .from(qChatRoom)
                 .leftJoin(qProduct).on(qProduct.id.eq(qChatRoom.productId))

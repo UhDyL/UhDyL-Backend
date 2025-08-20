f2952544a6558e839847c0358223fd4a6d44f8e6
kon28289 <kon28289@naver.com>
2025-08-20 12:45:40 +0900

    feat: 이미지 삭제를 위한 publicId 발급 API 추가

--- src/main/java/com/uhdyl/backend/review/repository/ReviewRepository.java
+++ src/main/java/com/uhdyl/backend/review/repository/ReviewRepository.java
@@ -3,7 +3,11 @@ package com.uhdyl.backend.review.repository;
 import com.uhdyl.backend.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long>, CustomReviewRepository {
     Review findByPublicId(String publicId);
     boolean existsByUser_IdAndPublicId(Long userId, String publicId);
+
+    Optional<Review> findByUser_IdAndImageUrl(Long userId, String imageUrl);
 }

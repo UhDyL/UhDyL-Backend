30390da64aefa4e37664a812dd2a250362222697
kon28289 <kon28289@naver.com>
2025-09-21 18:11:09 +0900

    feat: querydsl 수정

--- src/main/java/com/uhdyl/backend/product/repository/CustomProductRepositoryImpl.java
+++ src/main/java/com/uhdyl/backend/product/repository/CustomProductRepositoryImpl.java
@@ -205,6 +205,7 @@ public class CustomProductRepositoryImpl implements CustomProductRepository{
         QImage image = QImage.image;
         QUser user = QUser.user;
         QZzim zzim = QZzim.zzim;
+        QReview review = QReview.review;
 
         PathBuilder<Product> entityPath = new PathBuilder<>(Product.class, "product");
 
@@ -219,8 +220,15 @@ public class CustomProductRepositoryImpl implements CustomProductRepository{
                         user.picture.coalesce(""),
                         image.imageUrl,
                         product.isSale.not(),
-                        zzim.id.count()
-
+                        zzim.id.count(),
+                        JPAExpressions
+                                .select(review.rating.avg().coalesce(0.0))
+                                .from(review)
+                                .where(review.targetUserId.eq(user.id).and(review.productId.eq(product.id))),
+                        JPAExpressions
+                                .select(review.count())
+                                .from(review)
+                                .where(review.targetUserId.eq(product.user.id).and(review.productId.eq(product.id)))
                 ))
                 .from(product)
                 .leftJoin(product.images, image).on(image.imageOrder.eq(0L))
